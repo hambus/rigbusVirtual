d9c690162b9c6259b4bf37e5e29aa36c8214d7c8
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-03-18 20:39:02 -0500

    Got the port object working on core 3.1

---
+++ VirtualRigBus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace VirtualRigBus
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+       bool _continue;
+       SerialPort _serialPort;
+
+
+      SerialPort portClass = new SerialPort();
+      var ports = SerialPort.GetPortNames();
+      foreach( var port in ports)
+      {
+        Console.WriteLine($"{port}");
+      }
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
